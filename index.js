2a0578c9dc3111f58c158bdf0746a5d01f5ce57b
justreisz <108192532+justreisz@users.noreply.github.com>
2023-10-31 20:35:05 +0000

    1st commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function verificarPassword() {
+    var pass1 = login.pass.value;
+    var pass2 = login.pass2.value;
+    if (pass1 === pass2) {
+        var user = login.user.value;
+        alert("Bem-vindo ao five " + user);
+    } else {
+        alert("Reintroduza corretamente a password!");
+        login.pass.focus();
+    }
+}
