603d7d668cc0a75de0f92637e8d917d36a942610
justreisz <joaomfdreis@gmail.com>
2023-11-03 21:01:41 +0000

    3rd

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 function verificarPassword() {
     var pass1 = login.pass.value;
     var pass2 = login.pass2.value;
-    if (pass1 === pass2) {
-        var user = login.user.value;
-        alert("Bem-vindo ao five " + user);
-    } else {
-        alert("Reintroduza corretamente a password!");
-        login.pass.focus();
-    }
-}
+    var utilizador = login.user.value;
+    var mail = login.mail.value;
 
-/* Smooth scroll */
-var scroll = new SmoothScroll('.topnav a[href*="#"]', {
-	speed: 500
-});
+    if (utilizador == "" || pass1 == "" || mail == "" || pass2 == "") {
+        document.getElementById("erro").textContent = "*Preencha todos os campos";
+        return false;
+    } else if (!mail.includes("@gmail.com")) {
+        document.getElementById("erro").textContent = "*Insira um email válido";
+        login.mail.focus();
+        return false;
+    } else if (pass1.length < 6) {
+        document.getElementById("erro").textContent = "*A password tem de ter pelo menos 6 caracteres";
+        login.pass.focus();
+        return false;
+    } else if (pass1 != pass2) {
+        document.getElementById("erro").textContent = "*Introduza corretamente as passwords";
+        login.pass.focus();
+        return false;
+    }
+
+    document.getElementById("erro").textContent = "Bem-vindo ao five " + utilizador;
+    window.location.assign("index.html");
+
+    return false; 
+}
